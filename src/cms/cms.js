64b54bcf974a0d1858a54b5f7a84c061176b50b9
bokamix <janiakpol@gmail.com>
2019-10-07 11:47:58 +0200

    Config Product Preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,9 +5,12 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import ProductsPreview from './preview-templates/ProductsPreview'
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
+
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('products', ProductsPreview)
